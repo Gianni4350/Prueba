09cdc262bb5f6e4412b11f168d1aca66d76ff363
Gianni4350 <giannisussalla_23@hotmail.com>
2013-11-19 21:18:52 -0200

    Ssitema de Taxis
    Tiene creada solo las Ventanas

---
+++ Systema/src/Clases/Chofer.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Clases;
+import java.util.*;
+/**
+ *
+ * @author Gianni
+ */
+public class Chofer {
+    
+    String nombre;
+    String apellido;
+    int cedula;
+    Date fechanacimiento;
+    String direccion;
+    String email;
+    int id;
+    
+}
---
+++ Systema/src/systema/Systema.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package systema;
+
+/**
+ *
+ * @author Gianni
+ */
+public class Systema {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
